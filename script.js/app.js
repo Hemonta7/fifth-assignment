295632a8627d1ff889a8607445e55baf9e94314a
Hemonta <hemontanath138.com>
2024-02-22 23:24:02 +0600

    fix some issue

--- script.js/app.js
+++ script.js/app.js
@@ -1,88 +1,128 @@
 
 const allBtn = document.getElementsByClassName("btn-seat");
-let count=0;
+let count = 0;
 for (const btn of allBtn) {
-    
-        btn.addEventListener('click', function () {
-            if (count!==4) {
+
+    btn.addEventListener('click', function () {
+        if (count !== 4) {
             btn.style.backgroundColor = "#1DD100";
             condition();
             count++;
-            btn.disabled=true;
-            const tbody=document.getElementById("seatSummary");
-            const tr=document.createElement('tr');
-            const td=document.createElement('td');
-            const td1=document.createElement('td');
-            const td2=document.createElement('td');
-            td.innerText=btn.innerText;
-            td1.innerText="Economy";
-            td2.innerText="550";
+            btn.disabled = true;
+            const tbody = document.getElementById("seatSummary");
+            const tr = document.createElement('tr');
+            const td = document.createElement('td');
+            const td1 = document.createElement('td');
+            const td2 = document.createElement('td');
+            td.innerText = btn.innerText;
+            td1.innerText = "Economy";
+            td2.innerText = "550";
             tr.appendChild(td);
             tr.appendChild(td1);
             tr.appendChild(td2);
             tbody.appendChild(tr);
             totalCost();
-            grandTotal();
-            grandTotal(status);
             seatAvaliable();
 
-            }
-            
-            
-        })
-    }
-
-    function buyTicketButton() {
-        document.getElementById('paribahan').scrollIntoView({ behavior: 'smooth' });
-    }
-
-    function seatAvaliable(){
-        const phoneNumber=document.getElementById("phone");
-        phoneNumber.addEventListener("keyup",function(){
-
-            const seatUp=getConvertedValue("SeatIncrease");
-            if(seatUp!==0 && phoneNumber.value!=''){
-                const nextBtn=document.getElementById("btnNext");
-                nextBtn.removeAttribute("disabled");
-    
-            }
-        })
-    }
-
-
-    function grandTotal(status){
-        const totalCost=getConvertedValue("totalCost");
-        if(status){
-            const couponcode=document.getElementById("couponCode");
-            if(couponcode.value=='NEW 15'){
-                const discount=totalCost*0.15;
-                document.getElementById("grandTotal").innerText=totalCost-discount;
-            }else{
-                alert("please enter a valid coupon code")
-            }
         }
-        else if(status){
-            const couponcode=document.getElementById("couponCode");
-            if(couponcode.value=='Couple 20'){
-                const discount=totalCost*0.15;
-                document.getElementById("grandTotal").innerText=totalCost-discount;
-            }else{
-                alert("please enter a valid coupon code")
-            }
+
+
+    })
+}
+
+function buyTicketButton() {
+    document.getElementById('paribahan').scrollIntoView({ behavior: 'smooth' });
+}
+
+function seatAvaliable() {
+    const phoneNumber = document.getElementById("phone");
+    phoneNumber.addEventListener("keyup", function () {
+
+        const seatUp = getConvertedValue("SeatIncrease");
+        if (seatUp !== 0 && phoneNumber.value != '') {
+            const nextBtn = document.getElementById("btnNext");
+            nextBtn.removeAttribute("disabled");
+
         }
-        else{
-            
-            document.getElementById("grandTotal").innerText=totalCost;
-        }
+    })
+}
+
+//  document.getElementById("couponCode").addEventListener("keyup", function(){
+//     const totalCost = getConvertedValue("totalCost");
+
+//     const couponcode = document.getElementById("couponCode");
+//     if (couponcode.value == 'NEW 15') {
+//         const discount = totalCost * 0.15;
+//         document.getElementById("grandTotal").innerText = totalCost - discount;
+//     }
+
+//     else if (couponcode.value == 'Couple 20') {
+//         const discount = totalCost * 0.2;
+//         document.getElementById("grandTotal").innerText = totalCost - discount;
+//     } else {
+//         alert("please enter a valid coupon code")
+//         document.getElementById("grandTotal").innerText=totalCost;
+//     }
+//  })
+function grandTotal(status) {
+    const totalCost = getConvertedValue("totalCost");
+
+    const couponcode = document.getElementById("couponCode");
+    if (couponcode.value == 'NEW 15') {
+        const discount = totalCost * 0.15;
+        document.getElementById("grandTotal").innerText = totalCost - discount;
     }
 
+    else if (couponcode.value == 'Couple 20') {
+        const discount = totalCost * 0.2;
+        document.getElementById("grandTotal").innerText = totalCost - discount;
+    } else {
+        alert("please enter a valid coupon code")
+        document.getElementById("grandTotal").innerText=totalCost;
+    }
 
-function totalCost(){
-    const count=550;
-    const totalCost=getConvertedValue("totalCost");
-    let sum=totalCost+count;
-    document.getElementById("totalCost").innerText=sum;
+}
+
+
+
+
+
+
+
+// function grandTotal(status){
+//     const totalCost=getConvertedValue("totalCost");
+//     if(status){
+//         const couponcode=document.getElementById("couponCode");
+//         if(couponcode.value=='NEW 15'){
+//             const discount=totalCost*0.15;
+//             document.getElementById("grandTotal").innerText=totalCost-discount;
+//         }else{
+//             alert("please enter a valid coupon code")
+//         }
+//     }
+//     else if(status){
+//         const couponcode=document.getElementById("couponCode");
+//         if(couponcode.value=='Couple 20'){
+//             const discount=totalCost*0.15;
+//             document.getElementById("grandTotal").innerText=totalCost-discount;
+//         }else{
+//             alert("please enter a valid coupon code")
+//         }
+//     }
+//     else{
+
+//         document.getElementById("grandTotal").innerText=totalCost;
+//     }
+// }
+
+
+function totalCost() {
+    const count = 550;
+    const totalCost = getConvertedValue("totalCost");
+    let sum = totalCost + count;
+    document.getElementById("totalCost").innerText = sum;
     console.log(sum);
+    document.getElementById("grandTotal").innerText=sum;
 }
 
 
